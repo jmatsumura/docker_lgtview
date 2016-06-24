71c9bbbb2b822af78a6f2c4cd88ee4f277de34e6
James Matsumura <james.matsumura@gmail.com>
2016-06-24 10:18:01 -0400

    added in MySQL and Apache security options at install. next up https

--- build_lgtview.sh
+++ build_lgtview.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-# First, establish the following containers:
+# First prompt the user for whether they want to add
+# password protection for MySQL. 
+echo -n "Would you like to add a password for the root MySQL user? Please enter 'yes' or 'no': "
+read response 
+if [ "$response" = 'yes' ]; then
+	echo -ne "\nPlease enter the desired password for the root MySQL user: "
+	read -s password
+	echo -ne "\nPlease re-enter the password: "
+	read -s password_confirm
+
+	while [ "$password" != "$password_confirm" ]; do
+
+		echo -ne "\nPasswords do not match. Please re-enter the password: "
+		read -s password_confirm
+
+	done
+
+	echo -ne "\nNow setting a root password for MySQL. View the docker-compose.yml file if you lose this password."
+	sed -i "26s/ALLOW_EMPTY_PASSWORD: 1/ROOT_PASSWORD: $password_confirm/" ./docker-compose.yml
+	echo -e "\nDone setting root password for MySQL."
+fi
+
+echo "Going to build and run the Docker containers now......"
+# Now, establish the following Docker containers:
 # 1. dockerlgtview_LGTview_1
 #  - Houses the Apache server and LGTview related code
 # 2. dockerlgtview_mongo_1
@@ -13,18 +36,19 @@
 
 # Before attempting to interact with the containers, give them
 # a few seconds to truly initiate running and be accessible.
+echo "Docker containers done building and ready to go!"
 #sleep 10
 
 # Ask the user whether they want their site to be password protected
-echo -n "Would you like to add password protection to the LGTview site? Please enter 'yes' or 'no': "
+echo -ne "\nWould you like to add password protection for viewing access to the LGTview site? Please enter 'yes' or 'no': "
 read response 
 if [ "$response" = 'yes' ]; then
 
-		echo -ne "Please enter the desired username for accessing LGTview: "
+		echo -ne "\nPlease enter the desired username for accessing LGTview: "
 		read username
 		echo -ne "\nPlease enter the desired password for the username $username: "
 		read -s password
-		echo -ne "\nPlease re-enter the desired password: "
+		echo -ne "\nPlease re-enter the password: "
 		read -s password_confirm
 
 	while [ "$password" != "$password_confirm" ]; do
@@ -34,15 +58,20 @@ if [ "$response" = 'yes' ]; then
 
 	done
 
-	echo -ne "\nAdding username and password protection to the site, just one moment..."
+	echo -e "\nAdding username and password protection to the site, just one moment..."
 
-	# Go into the Apache container and configure for passwords
-	docker exec -it dockerlgtview_LGTview_1 "$password_confirm\\n$password_confirm" | sudo htpasswd -c /etc/apache2/.htpasswd "$username"
+	# Go into the Apache container and configure for passwords. Here, respond to the
+	# two interactive prompts using the supplied passwords.
+	docker exec -it dockerlgtview_LGTview_1 (printf '%s\n%s' "$password_confirm" "$password_confirm" && cat) | sudo htpasswd -c /etc/apache2/.htpasswd "$username"
+
+	# Now configure the necessary Apache confs to accommodate this protected setup.
 	docker exec -it dockerlgtview_LGTview_1 sed -i '178s/None/All/' /etc/apache2/apache2.conf
+	docker exec -it dockerlgtview_LGTview_1 printf '%s\n%s\n%s\n%s' 'AuthType Basic' 'AuthName "Restricted Content"' 'AuthUserFile /etc/apache2/.htpasswd' 'Require valid-user' >> /var/www/html/.htaccess
+
 	# Restart the Apache container with this new configuration
 	docker kill -signal="USR1" dockerlgtview_LGTview_1
 
-	echo -ne "\nLogin requirement added. Please save this information somewhere so that you do not lose access to the site.\n"
+	echo "Login requirement added. Please be sure to save this information somewhere so that you do not lose access to the site."
 fi
 
 
