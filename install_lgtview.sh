ad0534ec11d576c490490de7303c9b1d244d20cd
James Matsumura <james.matsumura@gmail.com>
2016-06-24 15:51:00 -0400

    using this commit to stage debugging process

--- install_lgtview.sh
+++ install_lgtview.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo -n "----------------------------------------------------------------------------------------------------"
+echo -e "\nWelcome to the LGTview installer. Please follow the prompts below that will help configure the security level of the site."
+
 # First configure MySQL with or without a password depending on what the user wants.
-echo -n "Would you like to add a password for the root MySQL user? Please enter 'yes' or 'no': "
+echo -n "----------------------------------------------------------------------------------------------------"
+echo -ne "\nWould you like to add a password for the root MySQL user? Please enter 'yes' or 'no': "
 read response 
 if [ "$response" = 'yes' ]; then
 	echo -ne "\nPlease enter the desired password for the root MySQL user: "
@@ -20,6 +24,7 @@ if [ "$response" = 'yes' ]; then
 	sed -i "26s/ALLOW_EMPTY_PASSWORD: 1/ROOT_PASSWORD: $password_confirm/" ./docker-compose.yml
 	echo -e "\nDone setting root password for MySQL."
 fi
+echo "----------------------------------------------------------------------------------------------------"
 
 echo "Going to build and run the Docker containers now......"
 
@@ -34,20 +39,18 @@ echo "Going to build and run the Docker containers now......"
 #  - Houses the MySQL server
 docker-compose up -d
 
-echo "Docker containers done building and ready to go! Please follow the rest of the installation prompts."
-
-# Before attempting to interact with the containers, give them
-# a few seconds to truly initiate running and be accessible.
-sleep 5
+echo "----------------------------------------------------------------------------------------------------"
+echo -n "Docker containers done building and ready to go! Please follow the rest of the installation prompts."
+echo -ne "\n----------------------------------------------------------------------------------------------------"
 
 # Ask the user whether they want their site to be password protected
 echo -ne "\nWould you like to add password protection for viewing access to the LGTview site? Please enter 'yes' or 'no': "
 read response 
 if [ "$response" = 'yes' ]; then
 
-		echo -ne "\nPlease enter the desired username for accessing LGTview: "
+		echo -ne "Please enter the desired username for accessing LGTview: "
 		read username
-		echo -ne "\nPlease enter the desired password for the username $username: "
+		echo -ne "Please enter the desired password for the username $username: "
 		read -s password
 		echo -ne "\nPlease re-enter the password: "
 		read -s password_confirm
@@ -63,64 +66,56 @@ if [ "$response" = 'yes' ]; then
 
 	# Go into the Apache container and configure for passwords. Here, respond to the
 	# two interactive prompts using the supplied passwords.
-	docker exec -it dockerlgtview_LGTview_1 printf '%s\n%s' "$password_confirm" "$password_confirm" && cat \
-		 | htpasswd -c /etc/apache2/.htpasswd "$username"
+	docker exec -it dockerlgtview_LGTview_1 htpasswd -cb /etc/apache2/.htpasswd "$username" "$password_confirm"
 
 	# Now configure the necessary Apache confs to accommodate this protected setup.
 	docker exec -it dockerlgtview_LGTview_1 sed -i '178s/None/All/' /etc/apache2/apache2.conf
-	docker exec -it dockerlgtview_LGTview_1 printf '%s\n%s\n%s\n%s' \
-		'AuthType Basic' \
-		'AuthName "Restricted Content"' \
-		'AuthUserFile /etc/apache2/.htpasswd' \
-		'Require valid-user' \
-		>> /var/www/html/.htaccess
+	# Just in case the installer has failed, clear the file and start anew
+	rm ./.htaccess
+	printf '%s\n%s\n%s\n%s' 'AuthType Basic' 'AuthName "Restricted Content"' 'AuthUserFile /etc/apache2/.htpasswd' 'Require valid-user' >> ./.htaccess
+	docker cp ./.htaccess dockerlgtview_LGTview_1:/var/www/html/.htaccess
 
 	# Restart the Apache container with this new configuration
-	docker kill -signal="USR1" dockerlgtview_LGTview_1
+	docker kill --signal="USR1" dockerlgtview_LGTview_1
 
 	echo "Login requirement added. Please be sure to save this information somewhere so that you do not lose access to the site."
 fi
 
+echo -e "----------------------------------------------------------------------------------------------------\n"
+
 # Can't think of a reason a user would not want https so just add
 # it in as default instead of prompting for it. 
 docker exec -it dockerlgtview_LGTview_1 a2enmod ssl
-docker kill -signal="USR1" dockerlgtview_LGTview_1
-docker exec -it dockerlgtview_LGTview_1 mkdir /etc/apache2/ssl
+docker kill --signal="USR1" dockerlgtview_LGTview_1
 
+echo -e "\n----------------------------------------------------------------------------------------------------"
 # Interact with the user to setup SSL
 echo "Please answer the following in order to setup SSL (https) for the site."
 echo -ne "\nCountry Name (2 letter code) [US]: "
 read country
-echo -ne "\nState or Province Name (full name) [New York]: "
+echo -ne "State or Province Name (full name) [New York]: "
 read state
-echo -ne "\nLocality Name (eg, city) [New York City]: "
+echo -ne "Locality Name (eg, city) [New York City]: "
 read city
-echo -ne "\nOrganization Name (eg, company) [University of Maryland]: "
+echo -ne "Organization Name (eg, company) [University of Maryland]: "
 read organization
-echo -ne "\nOrganizational Unit Name (eg, section) [Institute for Genome Sciences]: "
+echo -ne "Organizational Unit Name (eg, section) [Institute for Genome Sciences]: "
 read division
-echo -ne "\nYour email [best_email@domain.com]: "
-read email
-docker exec -it dockerlgtview_LGTview_1 printf '\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n' \
-	"$country" \
-	"$state" \
-	"$city" \
-	"$organization" \
-	"$division" \
-	"172.18.0.1:8080" \
-	"$email" \
-	&& cat | openssl req -x509 -nodes -days 1460 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.crt
+docker exec -it dockerlgtview_LGTview_1 openssl req -x509 -nodes -days 1460 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.crt \
+	-subj "/C=$country/ST=$statei/L=$city/O=$organization/OU=$division/CN=172.18.0.1:8080"
 
 # Modify the confs to use the newly generated SSL cert+key
 docker exec -it dockerlgtview_LGTview_1 sed -i '32s@/etc/ssl/certs/ssl-cert-snakeoil.pem@/etc/apache2/ssl/apache.crt@' /etc/apache2/sites-available/default-ssl.conf
 docker exec -it dockerlgtview_LGTview_1 sed -i '33s@/etc/ssl/private/ssl-cert-snakeoil.key@/etc/apache2/ssl/apache.key@' /etc/apache2/sites-available/default-ssl.conf
 docker exec -it dockerlgtview_LGTview_1 sed -i "3s/webmaster@localhost/$email/" /etc/apache2/sites-available/default-ssl.conf
-docker exec -it dockerlgtview_LGTview_1 sed -i "3a\n\t\tServerName 172.18.0.1:8080" /etc/apache2/sites-available/default-ssl.conf
-docker exec -it dockerlgtview_LGTview_1 sed -i "4a\n\t\tServerAlias localhost:8080" /etc/apache2/sites-available/default-ssl.conf
+docker exec -it dockerlgtview_LGTview_1 sed -i "3a\\\t\tServerName 172.18.0.1:8080" /etc/apache2/sites-available/default-ssl.conf
+docker exec -it dockerlgtview_LGTview_1 sed -i "4a\\\t\tServerAlias localhost:8080" /etc/apache2/sites-available/default-ssl.conf
 
 # Set this new SSL conf and restart Apache one last time. SSL should now be enabled
 docker exec -it dockerlgtview_LGTview_1 a2ensite default-ssl.conf
-docker kill -signal="USR1" dockerlgtview_LGTview_1
+docker kill --signal="USR1" dockerlgtview_LGTview_1
+echo -ne "SSL now implemented (access site through https)."
+echo -e "\n----------------------------------------------------------------------------------------------------"
 
 # Make sure the MySQL server container is up and running
 UP=$(docker ps -a | grep 'mysql' | grep 'Up' | wc -l);
@@ -130,7 +125,7 @@ while [ "$UP" -ne 1 ]; do
 done
 
 # Now populate the MySQL database to prepare for curation via TwinBLAST
-docker exec -it dockerlgtview_LGTview_1 perl /lgtview/bin/init_db.pl
+#docker exec -it dockerlgtview_LGTview_1 perl /lgtview/bin/init_db.pl
 
 # Make sure the MongoDB server container is up and running
 UP1=$(docker ps -a | grep 'mongo:2.6' | grep 'Up' | wc -l);
